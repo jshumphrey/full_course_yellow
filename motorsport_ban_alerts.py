c1b617ad7f8b2b316577ed523ab99a29973f01de
Jackson Humphrey <jackson.s.humphrey@gmail.com>
2023-05-09 11:43:29 -0500

    Initial commit.

---
+++ motorsport_ban_alerts.py
@@ -0,0 +1,19 @@
+"""This bot monitors various motorsport Discord servers for new instances
+where a user is banned from the server, or other critera as specified
+for that server.
+
+When a new ban is detected, a message is sent to the "Staff of Motorsport
+Discords" server, alerting all mod staff of the new ban, and providing
+information about whether the newly-banned user is present in any of the
+other servers."""
+
+
+import discord
+
+
+def main():
+    """Execute top-level functionality - load the token and start the bot."""
+
+
+if __name__ == "__main__":
+    main()
